ea61e382cc8aec4c3b18ae3377828a037887f9b1
shoolinat-cloud <shoolinat@gmail.com>
2026-09-20 09:15:20 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -10,11 +10,40 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 1,
     },
+
     {
       url: `${baseUrl}/about`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
+    },
+
+    {
+      url: `${baseUrl}/archives`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+
+    {
+      url: `${baseUrl}/editorial-board`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+
+    {
+      url: `${baseUrl}/become-reviewer`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
     },
   ];
 }
